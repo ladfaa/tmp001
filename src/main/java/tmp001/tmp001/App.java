a2bd37a5d54017e6a0bce8627054ab8b832b7f03
ladfaa <mark063mail@163.com>
2014-10-24 03:16:32 +0800

    init

---
+++ src/main/java/tmp001/tmp001/App.java
@@ -0,0 +1,13 @@
+package tmp001.tmp001;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
